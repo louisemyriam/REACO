e2dbda9c369059459e37814eda459ed3385e656a
louisemyriam <louisemyriam2303@gmail.com>
2026-02-09 16:02:37 +0100

    page d'appli

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
 import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
@@ -13,32 +14,51 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarStyle: {
+          backgroundColor:"#0b0b0b",
+          height: 75,
+          paddingBottom: 10,
+          paddingTop: 10,
+          borderTopColor:"202020",
+        },
+        tabBarActiveTintColor:'#F5C542',
+        tabBarInactiveTintColor:'#8c8c8c',
+        tabBarLabelStyle:{fontSize:12,},
         tabBarButton: HapticTab,
+       
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Acceuil',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="fildactualite"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Communaute',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="feed-outline" color={color} />,
         }}
       />
+      
+      <Tabs.Screen
+        name="bibliotheque"
+        options={{
+          title: 'Bibliothèque',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="book-outline" color={color} />,
+        }}
+        />
 
-      <Tabs.Screen
-        name="compte"
+    <Tabs.Screen
+        name="profil"
         options={{
-          title: 'Compte',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person-outline" color={color} />,
+          title: 'Profil',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="person-outline" color={color} />,
         }}
       />
+  
     </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
+  
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
