02b5918ca7389f865621f3326f49bab4a0869753
naneauxdenise <naneauxdenise@gmail.com>
2026-02-09 14:59:58 +0100

    rajout de dossier

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -28,6 +29,14 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="compte"
+        options={{
+          title: 'Compte',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person-outline" color={color} />,
         }}
       />
     </Tabs>
